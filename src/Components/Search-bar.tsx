53bf32abd137d8af37c6897248e7d6adb72cb887
Tinashe Muchineripi <73638846+Tinashe-Muchie@users.noreply.github.com>
2022-03-27 11:22:18 +0400

    search input and sellect styling

--- src/Components/Search-bar.tsx
+++ src/Components/Search-bar.tsx
@@ -2,34 +2,25 @@ import { FaRegBell } from "react-icons/fa";
 
 export const SEARCH_BAR = () => {
   return (
-  <div className="Search_bar_wrapper">
-    <form>
-      <input 
-        type="text"
-        placeholder="&#61442; Search..."
-      />
-      <select>
-        <option value="filter"> Filter </option>
-        <option value="patients"> Patients </option>
-        <option value="doctors"> Doctors </option>
-      </select>
-    </form>
-    <div>
-      <div className="Search_bar_bell">
-        <FaRegBell />
-      </div>
+    <div className="Search_bar_wrapper">
+      <form>
+        <input type="text" placeholder="&#61442; Search..." />
+        <select>
+          <option value="filter"> Filter </option>
+          <option value="patients"> Patients </option>
+          <option value="doctors"> Doctors </option>
+        </select>
+      </form>
       <div>
-        <span>
-
-        </span>
-        <span>
-          Tendai Kutswa
-        </span>
-        <span>
-          Admin Manager
-        </span>
+        <div className="Search_bar_bell">
+          <FaRegBell />
+        </div>
+        <div>
+          <span></span>
+          <span>Tendai Kutswa</span>
+          <span>Admin Manager</span>
+        </div>
       </div>
     </div>
-  </div>
   );
 };
