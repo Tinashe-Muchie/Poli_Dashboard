5028548709b918698fd418fbceb29c4aadc9f822
Tinashe Muchineripi <73638846+Tinashe-Muchie@users.noreply.github.com>
2022-03-27 10:57:52 +0400

    search bar inputs

--- src/Components/Search-bar.tsx
+++ src/Components/Search-bar.tsx
@@ -1,3 +1,35 @@
+import { FaRegBell } from "react-icons/fa";
+
 export const SEARCH_BAR = () => {
-  return <></>;
+  return (
+  <div className="Search_bar_wrapper">
+    <form>
+      <input 
+        type="text"
+        placeholder="&#61442; Search..."
+      />
+      <select>
+        <option value="filter"> Filter </option>
+        <option value="patients"> Patients </option>
+        <option value="doctors"> Doctors </option>
+      </select>
+    </form>
+    <div>
+      <div className="Search_bar_bell">
+        <FaRegBell />
+      </div>
+      <div>
+        <span>
+
+        </span>
+        <span>
+          Tendai Kutswa
+        </span>
+        <span>
+          Admin Manager
+        </span>
+      </div>
+    </div>
+  </div>
+  );
 };
