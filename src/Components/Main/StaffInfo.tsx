5ecd52ccbffe32a2f1f972a3cc8b4118e15f7472
Tinashe Muchineripi <73638846+Tinashe-Muchie@users.noreply.github.com>
2022-03-28 14:11:14 +0400

    staff info styling

--- src/Components/Main/StaffInfo.tsx
+++ src/Components/Main/StaffInfo.tsx
@@ -1,19 +1,44 @@
 import { ReactNode } from "react";
+import { FaBriefcaseMedical, FaStethoscope, FaUserNurse } from "react-icons/fa";
 
 interface staff_info_fc {
   id: number;
   icon: ReactNode;
   title: string;
+  total_num: number;
 }
 
-export const STAFF_INFO = () => {
-  return (
-    <div className="staff_info_wrapper">
-      <div>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
+const staff_info: staff_info_fc[] = [
+  {
+    id: 1,
+    icon: <FaUserNurse />,
+    title: "Total Patients",
+    total_num: 24908
+  },
+  {
+    id: 2,
+    icon: <FaStethoscope />,
+    title: "Total Doctors",
+    total_num: 5408
+  },
+  {
+    id: 3,
+    icon: <FaBriefcaseMedical />,
+    title: "Total Staff",
+    total_num: 1420
+  }
+];
+
+const staff = staff_info.map((info) => (
+  <li key={info.id} className="staff_info_list">
+    <span>{info.icon}</span>
+    <div>
+      <span>{info.title}</span>
+      <span>{info.total_num}</span>
     </div>
-  );
+  </li>
+));
+
+export const STAFF_INFO = () => {
+  return <ul className="staff_info_wrapper">{staff}</ul>;
 };
