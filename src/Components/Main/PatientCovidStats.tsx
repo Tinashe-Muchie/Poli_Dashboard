f20e0f29a2f0147e310b4a5b54f1f3194df70ebb
Tinashe Muchineripi <73638846+Tinashe-Muchie@users.noreply.github.com>
2022-03-29 12:21:16 +0400

    patient covid stats heading

--- src/Components/Main/PatientCovidStats.tsx
+++ src/Components/Main/PatientCovidStats.tsx
@@ -1,7 +1,13 @@
 export const PATIENT_STATS = () => {
   return (
     <div className="patient_stats_wrapper">
-
+      <div>
+        <span>Patients Covid Statistics</span>
+        <div>
+          <a href="/#">Month</a>
+          <a href="/#">Weekly</a>
+        </div>
+      </div>
     </div>
   );
 };
