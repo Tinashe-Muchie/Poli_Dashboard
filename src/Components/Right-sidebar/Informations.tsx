54b464d061629988e5bd46f2240830ddb094c75c
Tinashe Muchineripi <73638846+Tinashe-Muchie@users.noreply.github.com>
2022-04-05 14:11:20 +0400

    calendar initilization

--- src/Components/Right-sidebar/Informations.tsx
+++ src/Components/Right-sidebar/Informations.tsx
@@ -1,63 +1,7 @@
+import Calendar from 'react-calendar';
 import { BiDotsHorizontalRounded, BiChevronDown } from "react-icons/bi";
 
-interface monthsIF {
-  id: number;
-  title: string;
-}
 
-const Months: monthsIF[] = [
-  {
-    id: 1,
-    title: "Jan"
-  },
-  {
-    id: 2,
-    title: "Feb"
-  },
-  {
-    id: 3,
-    title: "Mar"
-  },
-  {
-    id: 4,
-    title: "Apr"
-  },
-  {
-    id: 5,
-    title: "May"
-  },
-  {
-    id: 6,
-    title: "Jun"
-  },
-  {
-    id: 7,
-    title: "Jul"
-  },
-  {
-    id: 8,
-    title: "Aug"
-  },
-  {
-    id: 9,
-    title: "Sep"
-  },
-  {
-    id: 10,
-    title: "Oct"
-  },
-  {
-    id: 11,
-    title: "Nov"
-  },
-  {
-    id: 12,
-    title: "Dec"
-  }
-];
-const MonthList = Months.map((month) => {
-  return <li key={month.id}>{month.title}</li>;
-});
 export const INFORMATION = () => {
   return (
     <div className="information_wrapper">
@@ -68,9 +12,7 @@ export const INFORMATION = () => {
         </span>
       </div>
       <div>
-        <ul>
-          {MonthList} <BiChevronDown />
-        </ul>
+       <Calendar />
       </div>
     </div>
   );
