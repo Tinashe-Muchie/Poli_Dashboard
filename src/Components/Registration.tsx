845ac16c0cc30a7698d0f5ce948602039a6ce11a
Tinashe Muchineripi <73638846+Tinashe-Muchie@users.noreply.github.com>
2022-04-22 23:01:21 +0400

    registration form changes

--- src/Components/Registration.tsx
+++ src/Components/Registration.tsx
@@ -1,3 +1,139 @@
+import { useReducer } from 'react';
+
+interface stateIF {
+    first_name: string;
+    last_name: string;
+    title: string;
+    date_of_birth: number | string;
+    mailing_address: number | string;
+    home_phone: number | string;
+    work_phone: number | string;
+    cell_phone: number | string;
+    email_address: number | string;
+}
+
+const initialState:stateIF = {
+  first_name: '',
+  last_name: '',
+  title: '',
+  date_of_birth: '',
+  mailing_address: '',
+  home_phone: '',
+  work_phone: '',
+  cell_phone: '',
+  email_address: ''
+}
+
+type ACTIONTYPE =
+  | { type: 'first_name', payload: string }
+  | { type: 'last_name', payload: string }
+  | { type: 'title', payload: string }
+  | { type: 'date_of_birth', payload: number | string}
+  | { type: 'mailing_address', payload: number | string}
+  | { type: 'home_phone', payload: number | string}
+  | { type: 'work-phone', payload: number | string}
+  | { type: 'cell_phone', payload: number | string}
+  | { type: 'email_address', payload: number | string};
+
+const reducer = (state: typeof initialState, action: ACTIONTYPE) => {
+  switch (action.type) {
+    case 'first_name':
+      return {first_name: state.first_name}
+  }
+}
+
 export const REGISTRATION = () => {
-  return <div className="registration"></div>;
+
+
+
+  return <div className="registration_wrapper">
+    <form>
+      <div>
+        <label>
+          First Name:
+          <input 
+            name="first_name"
+            type="text"
+          />
+        </label>
+        <label>
+          Last Name: 
+          <input
+            name="last_name"
+            type="text"
+          />
+        </label>
+        <label>
+          Title: 
+          <input
+            name="title"
+            type="text"
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Date of Birth: 
+            <input
+              name="date_of_birth"
+              type="date"
+            />
+        </label>
+        <label>
+          Gender: 
+            <select value="">
+              <option value="male"> Male </option>
+              <option value="female"> Female </option>
+            </select>
+        </label>
+      </div>
+      <label>
+        Mailing Adress:
+          <input
+            name="mailing_address" 
+            type="text"
+          />
+      </label>
+      <label>
+        Home Phone: 
+          <input
+            name="home_phone"
+            type="tel"
+          />
+      </label>
+      <label>
+        Work Phone:
+          <input 
+            name="work_phone"
+            type="tel"
+          />
+      </label>
+      <label>
+        Cell Phone:
+          <input
+            name="cell_phone" 
+            type="tel"
+          />
+      </label>
+      <label>
+        Marital Status:
+          <select value="" onChange={()=> {}}>
+            <option value="single"> Single </option>
+            <option value="married"> Married </option>
+            <option value="separated"> Separated </option>
+            <option value="divorced"> Divorced </option>
+            <option value="widowed"> Widowed </option>
+            <option value="unknown"> Unknown </option>
+          </select>
+      </label>
+      <label>
+        email address:
+          <input
+            name="email_address" 
+            type="text"
+          />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  </div>;
 };
