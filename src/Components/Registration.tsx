768a96fc02d4ddaeac4e14509f574ce29ce31851
Tinashe Muchineripi <73638846+Tinashe-Muchie@users.noreply.github.com>
2022-04-26 15:49:26 +0400

    react hoook form registration

--- src/Components/Registration.tsx
+++ src/Components/Registration.tsx
@@ -1,134 +1,58 @@
-import { useReducer } from 'react';
-import { MdAttractions } from 'react-icons/md';
-
-const initialState = {
-  first_name: '',
-  last_name: '',
-  title: '',
-  date_of_birth: '',
-  mailing_address: '',
-  home_phone: '',
-  work_phone: '',
-  cell_phone: '',
-  email_address: ''
-}
-
-type ACTIONTYPE =
-  | { type: 'first_name', payload: string }
-  | { type: 'last_name', payload: string }
-  | { type: 'title', payload: string }
-  | { type: 'date_of_birth', payload: number | string}
-  | { type: 'mailing_address', payload: number | string}
-  | { type: 'home_phone', payload: number | string}
-  | { type: 'work-phone', payload: number | string}
-  | { type: 'cell_phone', payload: number | string}
-  | { type: 'email_address', payload: number | string};
-
-const reducer = (state: typeof initialState, action: ACTIONTYPE) => {
-  switch (action.type) {
-    case 'first_name':
-      return {first_name: action.payload};
-    case 'last_name':
-      return {last_name: action.payload};
-    case 'title': 
-      return {title: action.payload};
-    default: 
-      throw new Error('Bad input')
-  }
-}
+import { useForm } from "react-hook-form";
 
 export const REGISTRATION = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
 
-  const [ state, dispatch ] = useReducer(reducer, initialState);
-
-  return <div className="registration_wrapper">
-    <form>
-      <div>
-        <label>
-          First Name:
-          <input 
-            name="first_name"
-            type="text"
-          />
-        </label>
-        <label>
-          Last Name: 
-          <input
-            name="last_name"
-            type="text"
-          />
-        </label>
-        <label>
-          Title: 
-          <input
-            name="title"
-            type="text"
-          />
-        </label>
-      </div>
-      <div>
-        <label>
-          Date of Birth: 
-            <input
-              name="date_of_birth"
-              type="date"
-            />
-        </label>
-        <label>
-          Gender: 
-            <select value="">
-              <option value="male"> Male </option>
-              <option value="female"> Female </option>
-            </select>
-        </label>
-      </div>
-      <label>
-        Mailing Adress:
-          <input
-            name="mailing_address" 
-            type="text"
-          />
-      </label>
-      <label>
-        Home Phone: 
-          <input
-            name="home_phone"
-            type="tel"
-          />
-      </label>
-      <label>
-        Work Phone:
-          <input 
-            name="work_phone"
-            type="tel"
-          />
-      </label>
-      <label>
-        Cell Phone:
-          <input
-            name="cell_phone" 
-            type="tel"
-          />
-      </label>
-      <label>
-        Marital Status:
-          <select value="" onChange={()=> {}}>
-            <option value="single"> Single </option>
-            <option value="married"> Married </option>
-            <option value="separated"> Separated </option>
-            <option value="divorced"> Divorced </option>
-            <option value="widowed"> Widowed </option>
-            <option value="unknown"> Unknown </option>
-          </select>
-      </label>
-      <label>
-        email address:
-          <input
-            name="email_address" 
-            type="text"
-          />
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
-  </div>;
+  return (
+    <div className="registration_wrapper">
+      <form
+        onSubmit={handleSubmit((data) => {
+          console.log(data);
+        })}
+      >
+        <input
+          {...register("first_name", { required: true })}
+          type="text"
+          placeholder="First Name"
+        />
+        {errors.first_name?.type === "required" && "First name is required"}
+        <input
+          {...register("last_name", { required: true })}
+          type="text"
+          placeholder="Last Name"
+        />
+        {errors.last_name?.type === "required" && "Last name is required"}
+        <input
+          {...register("title", { required: true })}
+          type="text"
+          placeholder="title"
+        />
+        {errors.title?.type === "required" && "title is required"}
+        <input {...register("date_of_birth", { required: true })} type="date" />
+        {errors.date_of_birth?.type === "required" && "title is required"}
+        <select {...register("gender", { required: true })}>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </select>
+        {errors.gender?.type === "required" && "title is required"}
+        <input
+          {...register("work_phone", { required: true })}
+          type="tel"
+          placeholder="+263 775 786 980"
+        />
+        {errors.work_phone?.type === "required" && "title is required"}
+        <input
+          {...register("email", { required: true })}
+          type="email"
+          placeholder="tinoe@gmail.com"
+        />
+        {errors.email?.type === "required" && "title is required"}
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 };
