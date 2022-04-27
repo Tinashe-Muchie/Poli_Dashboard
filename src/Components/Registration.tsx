c532a09cdbf55d30cb497a45bff655b37f858156
Tinashe Muchineripi <73638846+Tinashe-Muchie@users.noreply.github.com>
2022-04-27 17:14:22 +0400

    error display using paragraph

--- src/Components/Registration.tsx
+++ src/Components/Registration.tsx
@@ -19,39 +19,39 @@ export const REGISTRATION = () => {
           type="text"
           placeholder="First Name"
         />
-        {errors.first_name?.type === "required" && "First name is required"}
+        {errors.first_name?.type === "required" ? <p className="error">This input is required</p> :''}
         <input
           {...register("last_name", { required: true })}
           type="text"
           placeholder="Last Name"
         />
-        {errors.last_name?.type === "required" && "Last name is required"}
+        {errors.last_name?.type === "required" ? <p className="error">This input is required</p> :''}
         <input
           {...register("title", { required: true })}
           type="text"
           placeholder="title"
         />
-        {errors.title?.type === "required" && "title is required"}
+        {errors.title?.type === "required" ? <p>This input is required</p> :''}
         <input {...register("date_of_birth", { required: true })} type="date" />
-        {errors.date_of_birth?.type === "required" && "title is required"}
+        {errors.date_of_birth?.type === "required" ? <p className="error">This input is required</p> :''}
         <select {...register("gender", { required: true })}>
           <option value="male">Male</option>
           <option value="female">Female</option>
           <option value="other">Other</option>
         </select>
-        {errors.gender?.type === "required"}
+        {errors.gender?.type === "required" ? <p className="error">This input is required</p> :''}
         <input
           {...register("work_phone", { required: true })}
           type="tel"
           placeholder="+263 775 786 980"
         />
-        <p>{errors.work_phone?.type === "required" && "phone is required"}</p>
+        {errors.work_phone?.type === "required" ? <p className="error">This input is required</p> :''}
         <input
           {...register("email", { required: true })}
           type="email"
           placeholder="tinoe@gmail.com"
         />
-        <p>{errors.email?.type === "required" && "email is required"}</p>
+        {errors.email?.type === "required" ? <p className="error">This input is required</p> :''}
         <input type="submit" value="Submit" />
       </form>
     </div>
